5fba53631c05e4446c27c2dd47c4861e46aa1110
Morten Haraldsen <m.haraldsen@kezzler.com>
2020-12-23 08:30:01 +0100

    Add removed line

--- test/my2ch.sh
+++ test/my2ch.sh
@@ -2,3 +2,4 @@
 set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p "$DIR/logs"
+docker run --name my2ch --rm -v"$DIR/logs":/logs -v"$DIR/configs":/configs --network=test_db-network -e TZ=UTC ethlocom/my2ch:latest --home=/ "$@"
